adb133496c186a3ca745106f6c81ea822e9ce267
Chandu-Pusa <pusasrujan@gmail.com>
2026-08-01 13:38:54 +0530

    "getting started"

---
+++ FirstNonRepeating.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FirstNonRepeating {
+
+    public static String firstNonRepeating(String s) {
+
+        // Write your logic here
+        
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String result = firstNonRepeating(s);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
